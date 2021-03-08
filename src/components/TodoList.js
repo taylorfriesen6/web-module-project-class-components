e18c8a984858b69ee5106751b4ee914498a4750c
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-08 14:30:15 -0800

    successfully passing App state into TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,13 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <p>{this.props.list}</p>
+    )
+  }
+}
+
+export default TodoList;

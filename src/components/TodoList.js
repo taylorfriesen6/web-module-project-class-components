c798351566acbb63ed67c453990a8365a361950f
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-08 15:10:47 -0800

    added keys for list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,7 @@ class TodoList extends React.Component {
   render() {
     return (
       <div>
-        {this.props.list.map(todo => <Todo value={todo}/>)}
+        {this.props.list.map((todo, index) => <Todo value={todo} key={index}/>)}
       </div>
     )
   }
